2df0fff8d4a8a43d76fa31d429bd8d6c9657f144
Ben Kehoe <ben@kehoe.io>
2020-11-24 16:20:12 -0700

    cli-v2.13.0

---
+++ cli/src/aws_sso_util/__main__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Ben Kehoe
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+from .cli import cli
+
+cli(prog_name="python -m aws_sso_util") #pylint: disable=unexpected-keyword-arg,no-value-for-parameter
