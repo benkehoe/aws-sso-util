83a151b49f4743b8dddac9dd0721dc8581ff30a7
Ben Kehoe <ben@kehoe.io>
2020-12-20 10:03:25 -0700

    lib-v1.5: Undo changes that broke credential-process

--- lib/aws_sso_lib/__init__.py
+++ lib/aws_sso_lib/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.4.0'
+__version__ = '1.5.0'
 
 from .sso import get_boto3_session, login, list_available_accounts, list_available_roles
 from .assignments import Assignment, list_assignments
