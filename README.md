b9068d000f68f58a4944949c1a676a06d78c0f1d
Ben Kehoe <ben@kehoe.io>
2021-11-17 16:26:06 -0700

    cli: Added run-as command

--- README.md
+++ README.md
@@ -101,6 +101,18 @@ But the whole point of AWS SSO is that you log in once for *many* accounts and r
 You could have a particular account and role set up in your default profile, but I prefer not to have a default profile so that I'm always explicitly selecting a profile and never accidentally end up in the default by mistake.
 `aws-sso-util login` solves this problem by letting you *just log in* without having to think about where you'll be using those credentials.
 
+## Running one-off commands as a specific account and role
+
+Read the full docs for `aws-sso-util run-as` [here](docs/run-as.md).
+
+In general, in the AWS SSO world, you shouldn't be trying to manually set credentials in an environment, nor thinking about "logging in" to a particular account and role.
+You log in to *AWS SSO* once, and then *use* accounts and roles with that session.
+You should orient yourself around configuration profiles—use [`aws-sso-util configure populate`](configure.md) to set up profiles for every account and role you have access to, and then use either the `--profile` argument to tell a command to use a specific profile, or set the `AWS_PROFILE` environment variable to have all commands your shell use a particular profile unless they are told otherwise ([here's a shell function to help manage that env var](https://gist.github.com/benkehoe/0d2985e56059437e489314d021be3fbe)).
+
+However, there are times when it's useful to be able to run a command as a specific account and role, without needing a profile configured for it—or without knowing the profile name corresponding to the account and role.
+For this purpose, there's `aws-sso-util run-as`.
+Think of it as the shell equivalent to [`aws_sso_lib.get_boto3_session()`](lib/README.md#get_boto3_session).
+
 ## Debugging issues
 
 Read the full docs for `aws-sso-util check` [here](docs/check.md).
---
+++ cli/src/aws_sso_util/run_as.py
@@ -0,0 +1,94 @@
+import logging
+import sys
+import os
+from datetime import datetime
+import shlex
+import subprocess
+
+import click
+
+from aws_sso_lib.sso import get_boto3_session, login
+
+from .utils import configure_logging, get_instance, GetInstanceError
+
+LOGGER = logging.getLogger(__name__)
+
+@click.command("run-as", context_settings=dict(
+    ignore_unknown_options=True,
+))
+@click.option("--sso-start-url", "-u", metavar="URL", help="Your AWS SSO start URL")
+@click.option("--sso-region", metavar="REGION", help="The AWS region your AWS SSO instance is deployed in")
+@click.option("--account-id", "-a", "account", metavar="ACCOUNT_ID", help="The AWS account", required=True)
+@click.option("--role-name", "-r", "role", metavar="ROLE_NAME", help="The SSO role to assume in account", required=True)
+@click.option("--region", metavar="REGION", help="The AWS region")
+@click.option("--force-refresh", is_flag=True, help="Re-login")
+@click.option("--verbose", "-v", count=True)
+@click.argument("exec_args", nargs=-1, type=click.UNPROCESSED, required=True)
+def run_as(
+        sso_start_url,
+        sso_region,
+        account,
+        role,
+        region,
+        force_refresh,
+        verbose,
+        exec_args):
+    """Run a command as a specific account + role.
+    """
+
+    configure_logging(LOGGER, verbose)
+
+    try:
+        instance = get_instance(
+            sso_start_url,
+            sso_region,
+        )
+    except GetInstanceError as e:
+        LOGGER.fatal(str(e))
+        sys.exit(1)
+
+    login(instance.start_url, instance.region, force_refresh=force_refresh)
+
+    for key in ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_SESSION_TOKEN', 'AWS_PROFILE', 'AWS_DEFAULT_PROFILE']:
+        os.environ.pop(key, None)
+
+    session = get_boto3_session(
+        instance.start_url,
+        instance.region,
+        account,
+        role,
+        region=region,
+    )
+
+    session_credentials = session.get_credentials()
+
+    read_only_credentials = session_credentials.get_frozen_credentials()
+    access_key_id = read_only_credentials.access_key
+    secret_access_key = read_only_credentials.secret_key
+    session_token = read_only_credentials.token
+
+
+    expiration = None
+    if hasattr(session_credentials, '_expiry_time') and session_credentials._expiry_time:
+        if isinstance(session_credentials._expiry_time, datetime):
+            expiration = session_credentials._expiry_time
+        else:
+            LOGGER.debug("Expiration in session credentials is of type {}, not datetime".format(type(expiration)))
+
+    env_vars = {
+        'AWS_ACCESS_KEY_ID': access_key_id,
+        'AWS_SECRET_ACCESS_KEY': secret_access_key,
+    }
+    if session_token:
+        env_vars['AWS_SESSION_TOKEN'] = session_token
+    if expiration:
+        env_vars['AWS_CREDENTIALS_EXPIRATION'] = expiration.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    if session.region_name:
+        env_vars['AWS_DEFAULT_REGION'] = session.region_name
+
+    os.environ.update(env_vars)
+
+    command = ' '.join(shlex.quote(arg) for arg in exec_args)
+    result = subprocess.run(command, shell=True)
+    sys.exit(result.returncode)
