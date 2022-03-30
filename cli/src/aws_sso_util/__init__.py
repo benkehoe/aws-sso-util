dc021144c825940e50da1a241f9b1cc6a7d8322d
Ben Kehoe <ben@kehoe.io>
2022-03-29 19:26:42 -0600

    Release lib-v1.11 and cli-v4.28

--- cli/src/aws_sso_util/__init__.py
+++ cli/src/aws_sso_util/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '4.27.0' # change in pyproject.toml too
+__version__ = '4.28.0' # change in pyproject.toml too
